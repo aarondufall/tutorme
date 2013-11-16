3ec48ef56fe5861ebe45e426a61dc099b3e6e721
Aaron Dufall <aald212@gmail.com>
2013-11-16 11:11:53 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutorme::Application.config.secret_key_base = '9d36362ccbfe90ab19335d17ad0147e49e63a67cd13915bc06690bf8d28bfe0b87733885f15aa9af6d0b3d7e9bb6939a65826cc7ce308df387f4a397c7071717'
