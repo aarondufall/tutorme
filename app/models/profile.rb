6a3011962f2d92c1eace4e6e7270cea0eceef23b
Aaron Dufall <aald212@gmail.com>
2013-11-16 13:18:14 -0800

    Profile listings

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,9 @@
 class Profile < ActiveRecord::Base
 	has_many :educations 
 	has_many :subjects
+
+
+	def name
+		"#{first_name} #{last_name}"
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+schools = %w(Havard Stanford Yale UBC SFU)
+degrees = %w(Physics Commerce Marketing Law)
+specializations = ["Canadian Studies", "Zoology", "Statistics", "Oceanography"]
+subjects = ["The science of Harry Potter", "Pornograpy historian","The Vampire in Literature and Cinema",'Elvish, the language of "Lord of the Rings"', "UFOs In American Society"]
+
+
+10.times do 
+	profile = Profile.create( first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, bio: Faker::Lorem.paragraph(10) )
+	(1 + rand(4)).times do
+		profile.educations.create(school: schools.shuffle.first, degree: degrees.shuffle.first, specialization: specializations.shuffle.first)
+	end
+	(1 + rand(4)).times do
+		profile.subjects.create(name: subjects.shuffle.first, level: "Year #{1 + rand(12)}")
+	end
+end
